b8be09c54cfe1a60cc1fd7b633ae4b5b9d056ad4
민경욱 <rrkcl7733@naver.com>
2023-01-29 22:04:32 +0900

    테스트

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
-
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import os
 import http.server
 import socketserver
+import main
 
 from http import HTTPStatus
 
@@ -10,7 +11,8 @@ class Handler(http.server.SimpleHTTPRequestHandler):
         self.send_response(HTTPStatus.OK)
         self.end_headers()
         msg = 'Python is running on Qoddi! You requested %s' % self.path
-        self.wfile.write(msg.encode())
+        # self.wfile.write(msg.encode())
+        main.root()
 
 
 port = int(os.getenv('PORT', 8080))
