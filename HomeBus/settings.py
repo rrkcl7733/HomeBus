a91464fd758ca21aa55908e1339a6c77d79bf191
민경욱 <rrkcl7733@naver.com>
2023-02-14 20:55:46 +0900

    background_task 삭제

--- HomeBus/settings.py
+++ HomeBus/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pwa',
-    'background_task',
 ]
 
 MIDDLEWARE = [
--- static/src/app.js
+++
@@ -1,7 +0,0 @@
-function updateTime() {
-      var now = moment();
-      var currentTime = now.format('hh:mm:ss');
-      document.getElementById('current-time').innerHTML = currentTime;
-}
-
-setInterval(updateTime, 1000);
